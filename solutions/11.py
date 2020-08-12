28432289d7f5598746f9f73693d787a0252e42f6
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-08-12 23:20:06 +0600

    remove odd number from list update

--- solutions/11.py
+++ solutions/11.py
@@ -1,11 +1,10 @@
 thislist = [1,2,3,4,5,6,7,8,9,10]
 
-
 def rm_odd():
-    print(thislist)
     for num in thislist:
-     if (num%2)==0:
-         print(num)
+     if (num%2)!=0:
+        thislist.remove(num)
+    print(thislist)
 
 
 
