27e01cfbc3950926f6baa3a36a4e6517194e1cb1
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-08-20 23:33:21 +0600

    solution 7 for final

---
+++ solutions/7.py
@@ -0,0 +1,5 @@
+# Write a python script to take any number of 10 digits as command-line argument and find the addition of all even digits and the multiplication of all odd digits.
+# 	For example:
+#             Input:     8775258553
+#             Output:  Addition : 8 + 2 =10
+#                           Multiplication : 7 * 5 * 3 =105
